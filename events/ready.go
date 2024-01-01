2b2926afbaee8778d53017f6f0056e951db17e9f
Ben Forster <109809772+ben-forster@users.noreply.github.com>
2024-01-01 21:56:40 +0000

    Create ready.go

---
+++ events/ready.go
@@ -0,0 +1,16 @@
+package events
+
+import (
+	"nyx/logger"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func Ready(session *discordgo.Session, event *discordgo.Ready) {
+	err := session.UpdateListeningStatus("nyx help")
+	if err != nil {
+		logger.Logger.WarningF("[ERROR]: %s", err.Error())
+	}
+
+	logger.Logger.InfoF("[%s:%s]", session.State.User.Username, "is now online.")
+}
